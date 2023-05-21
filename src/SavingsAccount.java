9649817dd55b2f8a6e419cd90439aa65941d3db8
Eniola-Anishe <eniola.anishe@binghamuni.edu.ng>
2023-05-21 14:46:37 +0100

    Created the SavingsAccount Class

---
+++ src/SavingsAccount.java
@@ -0,0 +1,15 @@
+public class SavingsAccount extends Account{
+
+    @Override
+    public boolean withdrawal(int amount) {
+        boolean withdraw;
+        if(amount>20000){
+            withdraw=false;
+        }
+        else{
+            withdraw=true;
+            accountBalance=accountBalance-amount;
+        }
+        return withdraw;
+    }
+}
